622ad255efa1ad945d9aeebcc0b1277bf6612a04
Rainie33 <Rainie.huang0215@gmail.com>
2025-09-22 15:37:23 +0800

    [Add] Login

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,26 @@
+import './login.scss';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Welcome Back</h1>
+          <p>Hello Friend, Have a Good Day</p>
+          <span>Register an account</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
